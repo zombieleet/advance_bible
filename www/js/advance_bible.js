3317df76b936d0d9118dd892e5e15ff412685912
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-04-17 07:44:57 +0100

    add settings code to browserify

--- www/js/advance_bible.js
+++ www/js/advance_bible.js
@@ -2250,6 +2250,124 @@ $traceurRuntime.registerModule("../advance_bible.js", [], function() {
       }(ViewNote);
     }, {"./loadRequested.js": 9}],
     13: [function(require, module, exports) {
+      var Settings = function() {
+        function Settings() {
+          var settingsOnOf = document.querySelector('.bible-settings');
+          this.settingsOnOf = function() {
+            return settingsOnOf;
+          };
+        }
+        return ($traceurRuntime.createClass)(Settings, {
+          setSliding: function() {
+            this.settingsOnOf().addEventListener('click', function(e) {
+              var target = e.target;
+              if (target.getAttribute('class').includes("bible-on-of-Parent")) {
+                Settings.SetElementState(target, "sound-state");
+                return;
+              } else if (target.getAttribute('class').includes("bible-notification-Parent")) {
+                Settings.SetElementState(target, "notification-state");
+                return;
+              }
+            });
+            return this;
+          },
+          setValues: function() {
+            this.settingsOnOf().addEventListener('click', function(e) {
+              var target = e.target;
+              if (target.getAttribute('class').includes("setval")) {
+                var parent = target.parentNode;
+                var elShowValue = parent.previousElementSibling.previousElementSibling;
+                elShowValue.setAttribute('data-current', target.textContent);
+                elShowValue.textContent = target.textContent;
+                var role = parent.getAttribute('role');
+                switch (role) {
+                  case null:
+                    break;
+                  case "fontFamily":
+                    localStorage.setItem('font-family', elShowValue.getAttribute('data-current'));
+                    break;
+                  case "fontSize":
+                    localStorage.setItem('font-size', elShowValue.getAttribute('data-current'));
+                    break;
+                  case "volumeLevel":
+                    localStorage.setItem('sound-level', elShowValue.getAttribute('data-current'));
+                    break;
+                  case "textColor":
+                    localStorage.setItem('text-color', elShowValue.getAttribute('data-current'));
+                    break;
+                  case "backgroundColor":
+                    localStorage.setItem('background-color', elShowValue.getAttribute('data-current'));
+                    break;
+                  default:
+                }
+              }
+            });
+            return this;
+          }
+        }, {
+          NotificationCallback: function(storageCan, target) {
+            Notification.requestPermission(function(permission) {
+              Notification.requestPermission(function(permission) {
+                if (permission === 'granted') {
+                  var _notify = new Notification("You will be notified if your todo date arrives");
+                  return true;
+                } else if (permission === 'denied') {
+                  return false;
+                }
+                return false;
+              });
+            });
+          },
+          StyleTogglElement: function(target, elChild) {
+            target.setAttribute('data-color', "black");
+            target.setAttribute("data-current", "yes");
+            elChild.setAttribute('data-change-state', 'settings-state');
+            target.style["background-color"] = target.getAttribute('data-color');
+          },
+          SetElementState: function(target, storageCan) {
+            var elChild = target.children[0];
+            if (target.getAttribute('data-color') === "darkgrey") {
+              if (storageCan === "notification-state") {
+                if ("Notification" in window) {
+                  if (Settings.NotificationCallback(storageCan)) {
+                    Settings.StyleTogglElement(target, elChild);
+                  } else {
+                    Settings.SetStatusMessage("Notification can only be enabled if you delete the storage from your browser");
+                    return;
+                  }
+                } else {
+                  Settings.SetStatusMessage("Update Your Browser To Enable Notification");
+                  return;
+                }
+              } else {
+                Settings.StyleTogglElement(target, elChild);
+              }
+              localStorage.setItem(storageCan, target.getAttribute('data-current'));
+              return;
+            }
+            target.setAttribute('data-color', "darkgrey");
+            target.setAttribute('data-current', "no");
+            elChild.removeAttribute('data-change-state');
+            target.style["background-color"] = target.getAttribute('data-color');
+            localStorage.setItem(storageCan, target.getAttribute('data-current'));
+          },
+          SetStatusMessage: function(msg) {
+            if (((typeof msg === 'undefined' ? 'undefined' : $traceurRuntime.typeof(msg))) !== 'string') {
+              throw Error(("expected a string as an argument but got " + ((typeof msg === 'undefined' ? 'undefined' : $traceurRuntime.typeof(msg)))));
+            }
+            var bibleRep = document.querySelector(".bible-report");
+            bibleRep.innerHTML = msg;
+            bibleRep.setAttribute('style', 'visibility: visible');
+            setTimeout(function() {
+              bibleRep.removeAttribute('style');
+            }, 3000);
+          }
+        });
+      }();
+      var settings = new Settings();
+      settings.setSliding().setValues();
+    }, {}],
+    14: [function(require, module, exports) {
       var Todo = function() {
         function Todo() {
           var $__2 = this;
@@ -2468,7 +2586,7 @@ $traceurRuntime.registerModule("../advance_bible.js", [], function() {
       }();
       var td = new Todo();
     }, {}]
-  }, {}, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]);
+  }, {}, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
   return {};
 });
 $traceurRuntime.getModule("../advance_bible.js" + '');
